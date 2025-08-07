b9450c45bafd6265e55f0bc409b78417256a0ad2
N1ck1ra <nicolasmoraiscorrea506@gmail.com>
2025-08-06 21:23:26 -0300

    Delete Biblioteca directory

--- Biblioteca/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Livro livro = new Livro("Dom Casmurro", "Machado de Assis", 1899, 39.90);
-        livro.emprestar();
-        livro.devolver();
-        livro.aplicarDesconto(10);
-
-    }
-}
